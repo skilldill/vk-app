dbc00da35173a5ea6486d4338170d6cc853bbcf7
Alexander Lomonosov <skilldill@yandex.ru>
2020-02-12 00:32:48 +0500

    Updates

---
+++ precache-manifest.4053b783fb757fddf766a94e4449da29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b08f718c5bc40ac9bdf1577dead54f",
+    "url": "/vk-app/index.html"
+  },
+  {
+    "revision": "50fc244c93f3e9292125",
+    "url": "/vk-app/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "b1532d8a6024d25f19a2",
+    "url": "/vk-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "50fc244c93f3e9292125",
+    "url": "/vk-app/static/js/2.2a02e89d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/vk-app/static/js/2.2a02e89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1532d8a6024d25f19a2",
+    "url": "/vk-app/static/js/main.0b822955.chunk.js"
+  },
+  {
+    "revision": "bef4354691772f6f6257",
+    "url": "/vk-app/static/js/runtime-main.88aa461a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app/static/media/persik.4e1ec840.png"
+  }
+]);
